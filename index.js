12c3177010d4d325b5de25767bee11482a72ab06
faqihakih <faqihakih12@gmail.com>
2021-03-12 23:15:08 +0700

    4 - fix port and add dotenv

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require('dotenv').config({});
 const express = require("express");
 const app = express();
 const mainRoutes = require("./src/routers");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", mainRoutes);
+const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server Is running on port 3000");
 });
